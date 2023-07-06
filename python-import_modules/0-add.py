327157bf786bbceb10099fa66c04bafc821afe80
kuir-juach <k.thuch@alustudent.com>
2023-07-06 08:59:46 -0700

    updated files

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-import def add(a,b): from add_0.py
-a = 1
-b = 
 
+from add_0 import add
+
+a = 1
+b = 2
+if __name__ == '__main__':
+    print("{} + {} = {}".format(a, b, add(a, b)))
