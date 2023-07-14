9d1327708fadae5d3ff7ed4125ed350063a58800
kuir-juach <k.thuch@alustudent.com>
2023-07-14 02:16:07 -0700

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return my_list
+        return new_in_list
     elif idx >= len(my_list):
         return my_list
     else:
