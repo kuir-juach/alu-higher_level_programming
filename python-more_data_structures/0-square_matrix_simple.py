8bd179530283b7909b16b0c9df81fc7e4e0818e8
kuir-juach <k.thuch@alustudent.com>
2023-07-17 03:09:40 -0700

    updated file

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if not matrix:
-        return None 
-    retrun[[num * num for num in row] for row in matrix]
+        return None
+    return[[num * num for num in row] for row in matrix]
--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,6 +4,7 @@ def search_replace(my_list, search, replace):
     if not my_list:
         return new_list
     for i in range(len(my_list)):
-        if new_list[i]- == search:
-            new_list[i] == replace
+        if new_list[i] == search:
+            new_list[i] = replace
+
     return new_list
