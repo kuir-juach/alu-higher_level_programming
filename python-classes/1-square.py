da65993d6642bda1e48ada7dd7cc690b4079c97c
kuir-juach <k.thuch@alustudent.com>
2023-07-21 03:47:09 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"create a square"
+
+
 class Square:
+    ''' create a square
+    has a private instance attribute:size '''
+
+
     def __init__(square, size):
         square.size = size
 
