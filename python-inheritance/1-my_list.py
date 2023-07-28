56a8ae1bfe0955dc6c18c63ee0c445da7df1db58
kuir-juach <k.thuch@alustudent.com>
2023-07-28 03:33:34 -0700

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,4 @@
-#!/usr/bin/python3
-''' No modules imported '''
-
-
 class MyList(list):
-    '''Our base class is list '''
-
     def print_sorted(self):
-        ''' The function to print sorted list in ascending order '''
-        print(sorted(self))
+        sorted_list = sorted(self)
+        print(sorted_list)
