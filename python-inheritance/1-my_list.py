9b306d43d6acb09ad2290f4f8bf6c9a031666fbc
kuir-juach <k.thuch@alustudent.com>
2023-07-28 04:45:38 -0700

    Updated

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,6 @@
 class MyList(list):
     '''Our base class is list '''
 
-    def print_sorted(self):
+    def print__sorted(self):
         ''' The function to print sorted list in ascending order '''
         print(sorted(self))
