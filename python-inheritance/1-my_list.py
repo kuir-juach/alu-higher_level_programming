416a28cd606df6343d8a3a5e751244e9030a4484
kuir-juach <k.thuch@alustudent.com>
2023-07-28 03:42:08 -0700

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+'''The List functions are inherited by this file'''
+
+
 class MyList(list):
+    '''This Class Inherits Function List'''
+
     def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
+        '''the code below prints the list In a sorted manner '''
+        sorted_list = self[:]
+        sorted_list.sort()
+        print("{}".format(sorted_list))
