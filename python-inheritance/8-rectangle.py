bf4a04e6b9c5cf52fdc2a7001348b39b86754572
kuir-juach <k.thuch@alustudent.com>
2023-07-28 05:35:31 -0700

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" triangle module importing geometry """
+""" triangle Rectangle that inherits from Geometry """
 
 
 BaseGeometry = _import_("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ rectange class inherit from Base geometry """
+    """ rectange class tha  inherits from Base geometry """
 
     def _init_(self, width, height):
         """ init class """
