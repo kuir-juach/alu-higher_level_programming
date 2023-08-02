35d19ccccb128f4ea7415bc952329dc92002966d
kuir-juach <k.thuch@alustudent.com>
2023-08-02 08:23:48 -0700

    0-privileges.sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all the  privileges of MySQL users user_0d_1 and user_0d_2
+-- privileges are listed on the  localhost server
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
