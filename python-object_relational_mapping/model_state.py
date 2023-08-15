d1a9b9f5b65b83166ea90728a6dc2aed6dbcbf96
kuir-juach <k.thuch@alustudent.com>
2023-08-15 06:29:09 -0700

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-'''a script 14-model_city_fetch_by_state.py that prints
-all City objects from the database hbtn_0e_14_usa'''
-
+'''a python file that contains the class definition of a
+State and an instance Base = declarative_base()'''
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
-class City(Base):
-    '''class defination'''
-    __tablename__ = 'cities'
+class State(Base):
+    '''the class definition'''
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
