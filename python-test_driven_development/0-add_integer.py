03788700eb9a82b83ba733947c7fde0649e2d5f3
kuir-juach <k.thuch@alustudent.com>
2023-08-15 01:02:30 -0700

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-"""a function that adds 2 integers"""
+"""write a function to add 2 integers"""
 
 
 def add_integer(a, b=98):
-    """
-    Adds and returns sum of a and b
-    - Args :
-        a: int or float
-        b: int or float, default 98
-    """
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
-
-    if type(a) != int:
+    """add 2 integers or floats cast as integer"""
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-
-    return a + b 
+    return(int(a) + int(b)) 
