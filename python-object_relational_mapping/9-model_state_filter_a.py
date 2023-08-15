426efa4c51db85e6dae7812225afb754ca2206da
kuir-juach <k.thuch@alustudent.com>
2023-08-15 06:40:31 -0700

    updated

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""
-Lists all State objects that contain the letter a from the database
-"""
+'''a script that prints the first State
+object from the database hbtn_0e_6_usa'''
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from model_state import Base, State
 
-
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    Base.metadata.create_all(engine)
 
-    states = session.query(State).filter(State.name.like('%a%')).all()
+    session = Session(engine)
 
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    record = session.query(State).first()
+
+    if record:
+        print("{}: {}".format(record.__dict__['id'], record.__dict__['name']))
+    else:
+        print("Nothing")
+
+    session.close()
