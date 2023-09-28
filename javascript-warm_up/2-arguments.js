343892cbaa063ea67565904d06b2bde38210fbec
kuir-juach <k.thuch@alustudent.com>
2023-09-28 06:10:52 -0700

    2-arguments.js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const numLength = process.argv.length;
+if (numLength === 2) {
+	console.log('No argument')
+}	else if (numLength ===3){
+	console.log('Argument found');
+	else {
+	console.log('Argument found');
+}
